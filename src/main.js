e4e86ec39964a44b1727082661a7c5efe5c54207
LianYuguai <1652727694@qq.com>
2022-11-13 15:35:15 +0800

    项目初始化

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import count from './js/count'
+import sum from './js/sum'
+import "./css/index.css"
+import "./css/iconfont.css"
+import "./less/index.less"
+count();
+sum();
---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require("path")
+const ESLintPlugin = require('eslint-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  // 入口
+  entry: "./src/main.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/main.js",
+    // 打包前清空dist目录
+    clean: true
+  },
+  module: {
+    // loader
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.less$/,
+        use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+        }, {
+          loader: "css-loader" // translates CSS into CommonJS
+        }, {
+          loader: "less-loader" // compiles Less to CSS
+        }
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 500 * 1024 // 小于500kb 转base64,
+          }
+        },
+        generator: {
+          filename: "static/images/[hash:8][ext][query]"
+        }
+      },
+      {
+        test: /\.(ttf|woff2?|mp3|mp4|avi)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "static/media/[hash][ext][query]"
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ],
+
+  },
+  // 插件
+  plugins: [
+    new ESLintPlugin({
+      context: path.resolve(__dirname, 'src'),
+      exclude: 'node_modeules',
+      cache: true,
+      cacheLocation: path.resolve(__dirname, 'node_modules/.cache/eslints'),
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public/index.html')
+    })
+  ],
+  mode: "development"
+
+}
